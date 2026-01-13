eee333817b10ac12572b06d4a460cf952dce05a3
Nukually <317255300@qq.com>
2026-01-13 10:54:57 +0800

    Use venv in Windows build script

--- build-windows.bat
+++ build-windows.bat
@@ -1,6 +1,11 @@
 @echo off
 setlocal
 
+if not exist .venv (
+  python -m venv .venv
+)
+
+call .venv\Scripts\activate
 python -m pip install -r requirements.txt
 python -m pip install pyinstaller
 pyinstaller --onefile --name whosthemurder --add-data "data/scripts;data/scripts" -m frontend.main
